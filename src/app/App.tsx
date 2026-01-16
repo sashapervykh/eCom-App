38810cd2ed0fcee58ab35bfa6a8140a32685e5ee
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-01-16 18:38:20 +0300

    refactor: replace app.tsx to src/app/App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,20 +2,20 @@ import '@gravity-ui/uikit/styles/styles.css';
 import '@gravity-ui/uikit/styles/fonts.css';
 import { ThemeProvider, Toaster, ToasterComponent, ToasterProvider } from '@gravity-ui/uikit';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/main/main';
-import { NotFoundPage } from './pages/404/not-found';
-import { AboutPage } from './pages/about-us/about-us';
-import { MainLayout } from './components/layout/layout';
-import { CatalogPage } from './pages/catalog/catalog';
-import { RegistrationPage } from './pages/registration/registration';
-import { AuthProvider } from './components/hooks/useAuth';
-import { LoginPage } from './pages/login/login';
-import { ProductsProvider } from './components/hooks/useProducts';
-import { ProductPage } from './pages/product-page/product-page';
-import { UserPage } from './pages/user/UserPage';
-import { CartProvider } from './components/hooks/useCart';
-import { CartPage } from './pages/cart/cart-page';
-import { FiltersProvider } from './components/hooks/useFilters';
+import { HomePage } from '../pages/main/main';
+import { NotFoundPage } from '../pages/404/not-found';
+import { AboutPage } from '../pages/about-us/about-us';
+import { MainLayout } from '../components/layout/layout';
+import { CatalogPage } from '../pages/catalog/catalog';
+import { RegistrationPage } from '../pages/registration/registration';
+import { AuthProvider } from '../components/hooks/useAuth';
+import { LoginPage } from '../pages/login/login';
+import { ProductsProvider } from '../components/hooks/useProducts';
+import { ProductPage } from '../pages/product-page/product-page';
+import { UserPage } from '../pages/user/UserPage';
+import { CartProvider } from '../components/hooks/useCart';
+import { CartPage } from '../pages/cart/cart-page';
+import { FiltersProvider } from '../components/hooks/useFilters';
 
 export function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { App } from './app';
+import { App } from './app/App';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 
