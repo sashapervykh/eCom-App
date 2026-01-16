cf150d21d1830a43684120054a46f10dfc5a964d
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2026-01-16 18:49:18 +0300

    refactor: extract providers from App.tsx

---
+++ src/app/providers/AppProviders.tsx
@@ -0,0 +1,25 @@
+import { ThemeProvider, Toaster, ToasterProvider } from '@gravity-ui/uikit';
+import { ReactNode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from '../../components/hooks/useAuth';
+import { CartProvider } from '../../components/hooks/useCart';
+import { FiltersProvider } from '../../components/hooks/useFilters';
+import { ProductsProvider } from '../../components/hooks/useProducts';
+
+export function AppProviders({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider theme="light">
+      <ToasterProvider toaster={new Toaster()}>
+        <BrowserRouter>
+          <AuthProvider>
+            <CartProvider>
+              <FiltersProvider>
+                <ProductsProvider>{children}</ProductsProvider>
+              </FiltersProvider>
+            </CartProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </ToasterProvider>
+    </ThemeProvider>
+  );
+}
